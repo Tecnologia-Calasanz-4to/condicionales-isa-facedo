7fc30c40ef9875bc881b488e1910c1877f107614
isa-facedo <im.facedo@colegiocalasanz.edu.ar>
2026-05-06 13:14:43 -0300

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,7 @@
+temperatura= int (input ("Ingrese la temperatura que hay el dia de hoy "))
+if temperatura < 10:
+   print ("La temperatura es fria")
+elif 10 < temperatura < 25:
+   print ("La temperatura es agradable")
+else:
+   print ("La temperatura es calurosa")
